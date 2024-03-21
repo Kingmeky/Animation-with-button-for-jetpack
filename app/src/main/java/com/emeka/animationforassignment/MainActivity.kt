8d7ce49a4473fc4888e20719a140dc18c44e2488
kingmeky <emeka.andrew.ekeke@gmail.com>
2024-03-21 00:46:20 -0400

    creating buttons

---
+++ app/src/main/java/com/emeka/animationforassignment/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.emeka.animationforassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.emeka.animationforassignment.ui.theme.AnimationforassignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationforassignmentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+   // Text(
+     //   text = "Hello $name!",
+     //   modifier = modifier
+    //)
+    Row {
+        var count by remember { mutableStateOf(0) }
+        var score by remember { mutableStateOf(0) }
+        Button(onClick = { count++; score+100 }) {
+            Text("Add")
+        }
+        AnimatedContent(targetState = count) { targetCount ->
+            // Make sure to use `targetCount`, not `count`.
+            Text(text = "Count: $targetCount")
+        }
+        AnimatedContent(targetState = score) { targetScore ->
+            // Make sure to use `targetCount`, not `count`.
+            Text(text = "Game Score: $targetScore")
+        }
+
+        AnimatedContent(
+            targetState = count,
+            transitionSpec = {
+                // Compare the incoming number with the previous number.
+                if (targetState > initialState) {
+                    // If the target number is larger, it slides up and fades in
+                    // while the initial (smaller) number slides up and fades out.
+                    slideInVertically { height -> height } + fadeIn() with
+                            slideOutVertically { height -> -height } + fadeOut()
+                } else {
+                    // If the target number is smaller, it slides down and fades in
+                    // while the initial number slides down and fades out.
+                    slideInVertically { height -> -height } + fadeIn() with
+                            slideOutVertically { height -> height } + fadeOut()
+                }.using(
+                    // Disable clipping since the faded slide-in/out should
+                    // be displayed out of bounds.
+                    SizeTransform(clip = false)
+                )
+            }
+        ) { targetCount ->
+            Text(text = "$targetCount")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AnimationforassignmentTheme {
+        Greeting("Android")
+    }
+}
